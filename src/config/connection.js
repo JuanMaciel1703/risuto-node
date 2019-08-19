de6949ed82dd534c412ab871ccda7056fc8c6433
Juan Maciel <jmaciel172013@gmail.com>
2019-08-19 00:30:00 -0300

    Env vars fix

--- src/config/connection.js
+++ src/config/connection.js
@@ -3,7 +3,9 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@risuto-app-ope4b.mongodb.net/test?retryWrites=true&w=majority`, {
+console.log(process.env);
+
+mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@risuto-app-ope4b.mongodb.net/test?retryWrites=true&w=majority`, {
   useNewUrlParser: true
 });
 
